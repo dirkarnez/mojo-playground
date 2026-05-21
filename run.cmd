b20c4b596b3be768fba2617a6dc7013d0fb7504f
Dirk Arnez <smalldirkalex@gmail.com>
2026-05-21 19:06:55 +0800

    - update run.cmd

--- run.cmd
+++ run.cmd
@@ -3,12 +3,17 @@
 
 set DOWNLOADS_DIR=%USERPROFILE%\Downloads
 
-set PREFIX=D:\Softwares
-@REM set PREFIX=%DOWNLOADS_DIR%
+@REM set PREFIX=D:\Softwares
+set PREFIX=%DOWNLOADS_DIR%
 
 
-set PYTHON_DIR=%PREFIX%\python-3.13.9-amd64-portable
-set PATH=%PYTHON_DIR%;%PYTHON_DIR%\Scripts
+set PYTHON_DIR=^
+%PREFIX%\python-3.13.9-amd64-portable
+
+set PATH=^
+%PYTHON_DIR%;^
+%PYTHON_DIR%\Scripts;^
+%PREFIX%\pixi-x86_64-pc-windows-msvc;
 
 python main.py
 
